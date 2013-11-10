48bf1b3c0d298d04b8c0f46acc9088759641ab4a
wilariz <wilarizz@hotmail.com>
2013-11-11 00:33:13 +0100

    hierarchy ac

---
+++ ch2/src/main/java/com/apress/prospring3/ch4/SimpleTarget.java
@@ -0,0 +1,13 @@
+package com.apress.prospring3.ch4;
+
+public class SimpleTarget {
+	
+	private String val;
+	
+	public void setVal(String val) {
+		this.val = val;
+	}
+	public String getVal() {
+		return val;
+	}
+}
