2ec592f6f6fde775f4a65127304d502e95c265ab
wilariz <wilarizz@hotmail.com>
2013-11-11 23:26:59 +0100

    method injection

---
+++ ch2/src/main/java/com/apress/prospring3/ch4/ArtworkSender.java
@@ -0,0 +1,7 @@
+package com.apress.prospring3.ch4;
+
+public interface ArtworkSender {
+	//public void sendArtwork(String artworkPath, Recipient recipient);
+	public String getFriendlyName();
+	public String getShortName();
+}
---
+++ ch2/src/main/java/com/apress/prospring3/ch4/FtpArtworkSender.java
@@ -0,0 +1,19 @@
+package com.apress.prospring3.ch4;
+
+public class FtpArtworkSender implements ArtworkSender {
+
+//	public void sendArtwork(String artworkPath, Recipient recipient) {
+//		// ftp logic here...
+//	}
+	
+	public String getFriendlyName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getShortName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
